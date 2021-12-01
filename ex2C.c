3a05008e34564e4a01837d65150da3c34bd73b95
Gunnar <13799935+gunnarpieter@users.noreply.github.com>
2021-12-01 14:42:27 +0100

    Create ex2C.c

---
+++ ex2C.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void func(){
+    char password[16] = "supersecret";
+    char answer[16];
+
+    printf("What is your name?∖n");
+    gets(answer);
+
+    printf("Your name is: %s ∖n", answer);
+    printf("Your password is: %s ∖n", password);
+}
+
+void main(int argc, char **argv){
+    func();
+}
