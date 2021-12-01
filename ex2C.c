7745091a1c0e6a6c5fa7aa8c84562a238ebfdbde
Gunnar <13799935+gunnarpieter@users.noreply.github.com>
2021-12-01 14:44:33 +0100

    Update ex2C.c

--- ex2C.c
+++ ex2C.c
@@ -4,11 +4,11 @@ void func(){
     char password[16] = "supersecret";
     char answer[16];
 
-    printf("What is your name?∖n");
+    printf("What is your name?\n");
     gets(answer);
 
-    printf("Your name is: %s ∖n", answer);
-    printf("Your password is: %s ∖n", password);
+    printf("Your name is: %s \n", answer);
+    printf("Your password is: %s \n", password);
 }
 
 void main(int argc, char **argv){
